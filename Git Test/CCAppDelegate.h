327d2ed421b8876d8e38559f106207fffe2c5b39
Andrew Tweed <andrewtweed@Andrew-Mac.local>
2014-06-22 10:09:42 +0100

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by Andrew Tweed on 22/06/2014.
+//  Copyright (c) 2014 Andrew Tweed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Andrew Tweed on 22/06/2014.
+//  Copyright (c) 2014 Andrew Tweed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
