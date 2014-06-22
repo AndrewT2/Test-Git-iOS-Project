8eb1debea50a68a8642ea13ec447fe8872511179
Andrew Tweed <andrewtweed@Andrew-Mac.local>
2014-06-22 10:27:02 +0100

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Andrew Tweed on 22/06/2014.
+//  Copyright (c) 2014 Andrew Tweed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Andrew Tweed on 22/06/2014.
+//  Copyright (c) 2014 Andrew Tweed. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
